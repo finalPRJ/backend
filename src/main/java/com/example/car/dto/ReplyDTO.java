7cadf656a9b1c2dd1cd8f4b17d33db7522931511
taeyoung kang <kty1467@naver.com>
2023-05-13 20:09:57 +0900

    replyDTO 주석 추가

--- src/main/java/com/example/car/dto/ReplyDTO.java
+++ src/main/java/com/example/car/dto/ReplyDTO.java
@@ -9,10 +9,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Data
 public class ReplyDTO {
-    private Integer rno;
-    private String content;
-    private String replyerId;
-    private String replyerNickname;
-    private Integer bno;
-    private LocalDateTime createtime;
+    private Integer rno;  // 댓글 번호
+    private String content; // 댓글 내용
+    private String replyerId; // 댓글 단 사람 id
+    private String replyerNickname; // 댓글 단 사람 닉네임
+    private Integer bno; // 댓글이 달려있는 게시글 번호
+    private LocalDateTime createtime;  // 작성 시간
 }
